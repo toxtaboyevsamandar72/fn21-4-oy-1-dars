95419ec8d2d2621bb4be1925d4234fa1b737211f
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-02-11 23:09:29 +0500

    index html

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// alert('kirish')
+
+// console.log("hello");
+
+// let name = "samandar";
+// console.log(name);
+
+
+// let name1 = '2';
+// let name2 = '5';
+
+// let res = name1+name2;
+// console.log(res);
+
+// string
+
+
+// console.log('hello');
+
+// let email = 'samandar@gmail.com';
+// console.log(email);
+
+// string concatenation
+
+// let firstName= 'samandar';
+// let lastName = 'toxtaboyev';
+
+// let res = firstName + ' ' +lastName;
+// console.log(res);
+
+// getting Characters;
+
+// console.log(res[2]);
+
+// string length => uzunlik 
+
+
+// console.log(res.length);
+
+// string methods => funksiyaga oxshash   ish bajaradi;
+
+// console.log(res.toUpperCase());
+
+
+// let result = res.toLowerCase()
+
+// console.log(result);
+
+// let index = email.indexOf('@');
+// console.log(index);
